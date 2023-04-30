1a89915700cb42c97a2eec1ce5749309be4228b3
Christian López <chlopez89@gmail.com>
2023-04-30 17:40:50 -0600

    Modificaciones al Readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Platzi Scripts
+# Chlopez Scripts
 
 Dummy tests are in `jenkins-tests`
--- jenkins-tests/test/index.js
+++ jenkins-tests/test/index.js
@@ -2,13 +2,13 @@ const sum = require('../');
 const assert = require('assert');
 
 describe('Sum', () => {
-  it('should handle positive numbers', () => {
+    it('should handle positive numbers', () => {
     assert.equal(sum(3, 5), 8);
-  });
-  it('should handle negative numbers', () => {
+    });
+    it('should handle negative numbers', () => {
     assert.equal(sum(3, -5), -2);
-  });
-  it('should handle decimals', () => {
+    });
+    it('should handle decimals', () => {
     assert.equal(sum(3.5, 7.1), 10.6);
-  });
+    });
 });
